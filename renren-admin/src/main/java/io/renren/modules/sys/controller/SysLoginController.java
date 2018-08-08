e85020760da7b840a91560c5b83b89e052d13917
justsunzh <18506273747@163.com>
2018-08-08 10:31:53 +0800

    去掉验证码登陆

--- renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysLoginController.java
@@ -70,9 +70,10 @@ public class SysLoginController {
 	@RequestMapping(value = "/sys/login", method = RequestMethod.POST)
 	public R login(String username, String password, String captcha) {
 		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
-		if(!captcha.equalsIgnoreCase(kaptcha)){
+		//TODO 验证码被临时去掉了
+		/*if(!captcha.equalsIgnoreCase(kaptcha)){
 			return R.error("验证码不正确");
-		}
+		}*/
 		
 		try{
 			Subject subject = ShiroUtils.getSubject();
